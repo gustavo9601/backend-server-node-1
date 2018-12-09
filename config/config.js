497d1549e53ef5ce033427a6dcf1e2dc255c1bd6
gustavo9601 <tavo9601@gmail.com>
2018-12-08 23:58:07 -0500

    Autenticacion con google

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
 
 // Google
-module.exports.GOOGLE_CLIENT_ID = '442737206823-dilej5tevnrv61sovd7bocf5qeafmjs3.apps.googleusercontent.com';
-module.exports.GOOGLE_SECRET = '6cWrrFy8KchLLrYmg70esp7t';
+module.exports.GOOGLE_CLIENT_ID = '22928236553-bsaj1oovukioqifapq6d14srlrqrm5st.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'v_eSwLYQ52f9ia88Qqzn_9iA';
--- models/usuario.js
+++ models/usuario.js
@@ -9,16 +9,17 @@ var rolesValidos = {
     message: '{VALUE} no es un rol permitido'
 };
 
+
 var usuarioSchema = new Schema({
-    nombre: {type: String, required: [true, 'El nombre es necesario']},
-    email: {type: String, unique: true, required: [true, 'El email es necesario']},
-    password: {type: String, required: [true, 'La contraseña es necesaria']},
-    img: {type: String, required: false},
-    role: {type: String, required: true, default: 'USER_ROLE', enum: rolesValidos},
+
+    nombre: { type: String, required: [true, 'El nombre es necesario'] },
+    email: { type: String, unique: true, required: [true, 'El correo es necesario'] },
+    password: { type: String, required: [true, 'La contraseña es necesaria'] },
+    img: { type: String, required: false },
+    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos },
+    google: { type: Boolean, required: true, default: false }
 });
 
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
 
-usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser unico' });
-
-//Exportamos el modelo, con el nombre Usuario y le pasamos el objeto de modelo
 module.exports = mongoose.model('Usuario', usuarioSchema);
